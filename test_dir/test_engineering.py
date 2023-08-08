5d1eac86083e959957c73fd50316a0744d50d385
naivete13sky <chen320821@163.com>
2023-08-08 10:44:00 +0800

    20230808 3

--- test_dir/test_engineering.py
+++ test_dir/test_engineering.py
@@ -1,8 +1,11 @@
+import time
+
 import pytest
 from config import RunConfig
 from cc.cc_method import GetTestData
 from config_ep.epcam_ui import EPCAM
 
+from pywinauto.keyboard import send_keys
 
 @pytest.mark.input
 class TestUI:
@@ -15,8 +18,19 @@ class TestUI:
 
         # engineering_window.print_control_identifiers()
 
-        cc = EPCAM.get_engineering_left_top_Coor(engineering_window)
-        print("cc:",cc)
+        # cc = EPCAM.get_engineering_left_top_Coor(engineering_window)
+        # print("cc:",cc)
+
+        #清空料号，ctrl + A 全选料号，然后 ctrl + B删除
+        engineering_window.set_focus()# 激活窗口
+        send_keys("^a")# 发送 Ctrl + A 快捷键，全选
+        send_keys("^b")# 发送 Ctrl + B 快捷键，删除
+        send_keys("{ENTER}")# 发送回车键，删除
+        send_keys("{ENTER}")# 发送回车键，确认删除所有
+        time.sleep(3)
+
+
+
 
 
 
