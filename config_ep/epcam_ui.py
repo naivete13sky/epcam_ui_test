e0ad88f090e09997e663eefc70334e6181b91940
naivete13sky <chen320821@163.com>
2023-08-10 09:56:00 +0800

    20230810 4 ok

--- config_ep/epcam_ui.py
+++ config_ep/epcam_ui.py
@@ -38,7 +38,32 @@ def get_coor_of_object(text_wanted,text_from):
 
 class Engineering(object):
     def __init__(self):
-        self.engineering_window = RunConfig.driver_epcam_ui.window(**EPCAMPageInfo.engineering_window_para)
+        self.set_default_ui_para()
+        self.engineering_window = RunConfig.driver_epcam_ui.window(**self.engineering_window_para)
+
+
+    def set_default_ui_para(self):
+        pass
+        # engineering_window
+        self.engineering_window_para = {'title': "Engineering 1.1.7.2"}
+
+        # import job窗口
+        self.engineering_import_window_child_window_para = {'title': "Import Job", 'control_type': "Window"}
+
+        # import job窗口，获得选择文件类型控件
+        self.engineering_import_input_path_file_type_window_para = {'title': "文件类型(T):", 'auto_id': "1136",
+                                                               'control_type': "ComboBox"}
+
+        # 选择文件窗口中的文件路径对象
+        self.engineering_import_input_path_file_path_window_para = {'title': "文件名(N):", 'auto_id': "1148",
+                                                               'control_type': "ComboBox"}
+
+        # 确认选择中的文件
+        self.engineering_import_input_path_confirm_window_para = {'title': "打开(O)", 'auto_id': "1",
+                                                             'control_type': "Button"}
+
+
+
 
     # Engineering的方法
     def job_first_is_opened(self):
@@ -123,7 +148,7 @@ class Engineering(object):
         self.engineering_window.click_input(coords=self.get_engineering_import_Coor(coor_type='relative'))  # 使用鼠标单击按钮，无需主动激活窗口
 
         # 切换到import job窗口，并点击菜单input path
-        engineering_import_window = self.engineering_window.child_window(**EPCAMPageInfo.engineering_import_window_child_window_para)
+        engineering_import_window = self.engineering_window.child_window(**self.engineering_import_window_child_window_para)
         engineering_import_window.click_input(coords=self.get_engineering_import_input_path_Coor())# 点击菜单input path
 
 
@@ -131,21 +156,21 @@ class Engineering(object):
 
 
         # 获得选择文件类型控件，并选择ipc2581类型
-        engineering_import_input_path_file_type_window=self.engineering_window.child_window(**EPCAMPageInfo.engineering_import_input_path_file_type_window_para)
+        engineering_import_input_path_file_type_window=self.engineering_window.child_window(**self.engineering_import_input_path_file_type_window_para)
         engineering_import_input_path_file_type_window.select("ipc2581(*.xml *.cvg)")#根据文本选择下拉列表项
 
         # 选择文件对象
-        engineering_import_input_path_file_path_window=self.engineering_window.child_window(**EPCAMPageInfo.engineering_import_input_path_file_path_window_para)
+        engineering_import_input_path_file_path_window=self.engineering_window.child_window(**self.engineering_import_input_path_file_path_window_para)
         engineering_import_input_path_file_path_window.draw_outline(colour = 'red')# 绘制边框以突出显示控件（可以自行指定颜色和宽度）
         engineering_import_input_path_file_path_window.click_input()  # 将焦点设置到控件上
         send_keys(file_path)  # 输入文本
 
         # 确认选择中的文件
-        engineering_import_input_path_confirm_window=self.engineering_window.child_window(**EPCAMPageInfo.engineering_import_input_path_confirm_window_para)
+        engineering_import_input_path_confirm_window=self.engineering_window.child_window(**self.engineering_import_input_path_confirm_window_para)
         engineering_import_input_path_confirm_window.click_input()
 
         # 切换到import job窗口
-        engineering_import_window = self.engineering_window.child_window(**EPCAMPageInfo.engineering_import_window_child_window_para)
+        engineering_import_window = self.engineering_window.child_window(**self.engineering_import_window_child_window_para)
         engineering_import_window.click_input(coords=self.get_engineering_import_ok_Coor())
         send_keys("{ENTER}")
 
@@ -240,21 +265,6 @@ class Engineering(object):
             return (x, y)
 
 
-class EPCAMPageInfo(object):
-
-    # engineering_window
-    engineering_window_para = {'title':"Engineering 1.1.7.2"}
 
 
-    # import job窗口
-    engineering_import_window_child_window_para = {'title':"Import Job", 'control_type':"Window"}
-
-    # import job窗口，获得选择文件类型控件
-    engineering_import_input_path_file_type_window_para = {'title':"文件类型(T):",'auto_id':"1136",'control_type':"ComboBox"}
-
-    # 选择文件窗口中的文件路径对象
-    engineering_import_input_path_file_path_window_para = {'title':"文件名(N):",'auto_id':"1148",'control_type':"ComboBox"}
-
-    # 确认选择中的文件
-    engineering_import_input_path_confirm_window_para = {'title':"打开(O)", 'auto_id':"1",'control_type':"Button"}
 
