4f2ee82a5485244073a945e84f37b3a1bfbc0edb
naivete13sky <chen320821@163.com>
2023-08-10 17:02:57 +0800

    20230810 16 ok

--- test_dir/test_engineering.py
+++ test_dir/test_engineering.py
@@ -74,7 +74,11 @@ class TestUI:
         # my_engineering.engineering_window.print_control_identifiers()
 
     def test_option_language(self,epcam_ui_start):
-        pass
+        '''
+        禅道用例ID：1506
+        :param epcam_ui_start:
+        :return:
+        '''
         my_engineering = Engineering()
 
         my_engineering.language_switch(language='Simplified Chinese')
@@ -83,8 +87,12 @@ class TestUI:
 
         my_engineering.language_switch(language='EP Default')
 
-    @pytest.mark.coding
-    def test_file_save(self,epcam_ui_start):
+    def test_file_save_no_job_select(self,epcam_ui_start):
+        '''
+        禅道用例ID：3552
+        :param epcam_ui_start:
+        :return:
+        '''
         my_engineering = Engineering()
         my_engineering.engineering_window.click_input(coords=(800,800))#鼠标点击空白处，不选择料号
         my_engineering.file_save()
@@ -94,8 +102,8 @@ class TestUI:
         engineering_file_save_job_no_select_dialog = RunConfig.driver_epcam_ui.window(**{'title': "Save", 'control_type': "Window"})
         # dialog.print_control_identifiers()
         engineering_file_save_job_no_select_jpg = engineering_file_save_job_no_select_dialog.capture_as_image()# 截图
-        engineering_file_save_job_no_select_jpg.save(r'C:\cc\share\temp\engineering_file_save_job_no_select.png')
-        img = cv2.imread(r'C:\cc\share\temp\engineering_file_save_job_no_select.png')
+        engineering_file_save_job_no_select_jpg.save(r'C:\cc\share\temp\engineering_file_save_no_job_select.png')
+        img = cv2.imread(r'C:\cc\share\temp\engineering_file_save_no_job_select.png')
         img_cut = img[35:60, 35:205]  # 后面的是水平方向
         cv2.imwrite(r"C:\cc\share\temp\engineering_file_save_job_no_select_text.png", img_cut)
         cv2.waitKey(0)
@@ -108,5 +116,39 @@ class TestUI:
 
         send_keys("{ENTER}")#确认关闭弹窗
 
+    @pytest.mark.coding
+    @pytest.mark.skip
+    @pytest.mark.parametrize("job_id", GetTestData().get_job_id('Engineering'))
+    def test_file_save_no_job_select2(self,job_id,epcam_ui_start):
+        '''
+        禅道用例ID：3553。当前禅道中的用例写的有问题。
+        :param epcam_ui_start:
+        :return:
+        '''
+
+        # 下载料号
+        temp_path = os.path.join(r'C:\cc\share\temp', str(job_id))
+        if os.path.exists(temp_path):
+            # 删除目录及其内容
+            # os.remove(temp_path)
+            shutil.rmtree(temp_path)
+        os.mkdir(temp_path)
+        temp_compressed_path = os.path.join(temp_path, 'compressed')
+        job_current_all_fields = DMS().get_job_fields_from_dms_db_pandas(job_id)
+        file_compressed_name = job_current_all_fields['file'].split("/")[1]
+        DMS().file_downloand(os.path.join(temp_compressed_path, file_compressed_name), temp_compressed_path)
+
+        file_compressed_path = Path(os.path.join(temp_compressed_path, file_compressed_name))  # 替换为你的文件路径
+        job_name = file_compressed_path.stem
+
+        my_engineering = Engineering()
+        my_engineering.entity_filter(job_name)  # 筛选料号，在界面上显示指定某一个料号
+        if my_engineering.job_first_is_opened():
+            my_engineering.close_job_first()
+        my_engineering.delete_all_jobs()  # 删除筛选出的料号
+        my_engineering.import_ipc2581(str(file_compressed_path))  # 导入一个料号
+
+        my_engineering.open_job_first_by_double_click()  # 双击打开料号
+        my_engineering.go_up()  # 鼠标点击
 
 
