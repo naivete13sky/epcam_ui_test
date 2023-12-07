dc774ca01c483f45976fa4e00ae8b4cfa86c88f1
Administrator <563386606@qq.com>
2023-12-07 15:36:24 +0800

    20231207 zjr 1 ok matrix 1 case

--- test_dir/test_matrix.py
+++ test_dir/test_matrix.py
@@ -35,10 +35,11 @@ class Test_Matrix_UI:
         self.engineering.open_matrix_by_double_click() # 双击Matrix,打开Matrix窗口
         self.matrix = PageMatrix()
         job_info = {}
-        matrix_file_path = MyODB.get_odb_matrix_file_path(file_compressed_path)  # 获取odb的layer信息
-        job_info['layer_info'] = MyODB.get_layer_info_from_odb_file(matrix_file_path)
+        matrix_file_path = MyODB.get_odb_matrix_file_path(file_compressed_path)  # 得到odb的matrix文件路径
+        job_info['layer_info'] = MyODB.get_layer_info_from_odb_file(matrix_file_path) # 得到料号layer信息
 
         self.matrix.change_drill_cross(job_info,'drl1-10','top','bot') # 修改孔带
+
         img_name = 'drill_correlation_layer'
         cut_coords = [100, 799, 8, 1032] # 后面的是水平方向
         save_path_cut = self.matrix.cut_img(img_name, cut_coords)
@@ -46,115 +47,3 @@ class Test_Matrix_UI:
         assert self.matrix.is_right(save_path_cut, img_standard_str)
         self.matrix.close()  # 关闭matrix窗口
         self.engineering.go_up()  # 鼠标双击go_up
-
-    @pytest.mark.coding
-    @pytest.mark.parametrize("job_id", GetTestData().get_job_id('Change_Matrix'))
-    def test_matrix_double_click_layer_has_step(self, job_id, epcam_ui_start,
-                                                   download_file_compressed_entity_filter_delete_all_jobs_import):
-        """
-        双击有step名的对应layer单元格
-        禅道用例ID：4194
-        DMS_ID：42983
-        :param job_id:
-        :param epcam_ui_start:
-        :return:
-        """
-        download_file_compressed_entity_filter_delete_all_jobs_import(job_id)  # 调用 fixture 并传递参数值,下载料号
-        self.engineering.open_job_first_by_double_click()  # 双击打开料号
-        self.engineering.open_matrix_by_double_click()  # 双击Matrix,打开Matrix窗口
-
-        self.matrix = PageMatrix()
-        self.matrix.double_click_layer_has_step() # 双击有step的layer单元格
-
-        self.view_graphic = PageViewGraphic()
-        assert self.view_graphic.is_right()
-        self.view_graphic.close()
-
-        self.matrix.close()
-        self.engineering.go_up()
-
-    @pytest.mark.coding
-    @pytest.mark.parametrize("job_id", GetTestData().get_job_id('Change_Matrix'))
-    def test_matrix_double_click_step(self, job_id, epcam_ui_start,
-                                                download_file_compressed_entity_filter_delete_all_jobs_import):
-        """
-        双击有step名的对应layer单元格
-        禅道用例ID：4193
-        DMS_ID：42983
-        :param job_id:
-        :param epcam_ui_start:
-        :return:
-        """
-        download_file_compressed_entity_filter_delete_all_jobs_import(job_id)  # 调用 fixture 并传递参数值,下载料号
-        self.engineering.open_job_first_by_double_click()  # 双击打开料号
-        self.engineering.open_matrix_by_double_click()  # 双击Matrix,打开Matrix窗口
-
-        self.matrix = PageMatrix()
-        self.matrix.double_click_step()  # 双击有step的layer单元格
-
-        self.graphic = PageGraphic()
-        img_name = 'matrix_to_graphic_window'
-        cute_coords = [25, 799, 8, 1374] # 截图坐标，后面的是水平方向
-        img_standard_str = "graphic\\" + img_name + "_cut_standard.png"
-        self.graphic.is_right(img_name, cute_coords, img_standard_str)
-
-        self.graphic.close()
-        self.matrix.close()
-        self.engineering.go_up()
-
-    @pytest.mark.coding
-    @pytest.mark.parametrize("job_id", GetTestData().get_job_id('Change_Matrix'))
-    def test_matrix_click_layer(self, job_id, epcam_ui_start,
-                                      download_file_compressed_entity_filter_delete_all_jobs_import):
-        """
-        单击layer名后显示工具栏
-        禅道用例ID：4182
-        DMS_ID：42983
-        :param job_id:
-        :param epcam_ui_start:
-        :return:
-        """
-        download_file_compressed_entity_filter_delete_all_jobs_import(job_id)  # 调用 fixture 并传递参数值,下载料号
-        self.engineering.open_job_first_by_double_click()  # 双击打开料号
-        self.engineering.open_matrix_by_double_click()  # 双击Matrix,打开Matrix窗口
-
-        self.matrix = PageMatrix()
-        self.matrix.click_layer()  # 单击layer
-
-        img_name = 'click_layer'
-        cut_coords = [25, 799, 8, 1032]
-        img_standard_str = "matrix\\" + img_name + "_cut_standard.png"
-        assert self.matrix.is_right(img_name, cut_coords, img_standard_str)
-
-        self.matrix.close()
-        self.engineering.go_up()
-
-    @pytest.mark.coding
-    @pytest.mark.parametrize("job_id", GetTestData().get_job_id('Change_Matrix'))
-    def test_matrix_rectangle_selection_layer(self, job_id, epcam_ui_start,
-                                download_file_compressed_entity_filter_delete_all_jobs_import):
-        """
-        选中多行layer后显示工具栏
-        禅道用例ID：4183
-        DMS_ID：42983
-        :param job_id:
-        :param epcam_ui_start:
-        :return:
-        """
-        download_file_compressed_entity_filter_delete_all_jobs_import(job_id)  # 调用 fixture 并传递参数值,下载料号
-        self.engineering.open_job_first_by_double_click()  # 双击打开料号
-        self.engineering.open_matrix_by_double_click()  # 双击Matrix,打开Matrix窗口
-
-        self.matrix = PageMatrix()
-        self.matrix.click_layer()  # 单击layer
-        pyautogui.mouseDown(button='left')
-        pyautogui.moveTo(550, 670, duration=2)
-        pyautogui.mouseUp(button='left')
-
-        img_name = 'rectangle_selection_layer'
-        cut_coords = [25, 799, 8, 1032]
-        img_standard_str = "matrix\\" + img_name + "_cut_standard.png"
-        self.matrix.is_right(img_name, cut_coords, img_standard_str)
-
-        self.matrix.close()
-        self.engineering.go_up()
